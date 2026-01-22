0730f4f48a4b89cb472ad017f3e75330fd1a81f2
Alejandro Patiño <dapatinoa@unal.edu.co>
2026-01-22 09:14:37 -0500

    sss

--- src/utils/printUtils.js
+++ src/utils/printUtils.js
@@ -34,7 +34,6 @@ export const handleNativePrint = (elementId, title = 'Document') => {
         <head>
             <title>${title}</title>
             <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
             <style>
                 body {
                     background-color: white;
